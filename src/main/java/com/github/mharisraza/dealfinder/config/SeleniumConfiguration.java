7bfdc3ad055947f92509496180a2e17c85b7b0d0
Muhammad Haris <wannaheadshot5@gmail.com>
2024-02-14 23:14:04 +0500

    added selenium configuration

---
+++ src/main/java/com/github/mharisraza/dealfinder/config/SeleniumConfiguration.java
@@ -0,0 +1,23 @@
+package com.github.mharisraza.dealfinder.config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumConfiguration {
+
+    /*
+     * setup webDriver with headless mode.
+     * used PageLoadStrategy.EAGER to load page eagerly.
+     */
+    public static WebDriver setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        chromeOptions.addArguments("--headless=new");
+        return new ChromeDriver(chromeOptions);
+    }
+}
